735645ca89fb1d4a02e6c39310f7cafcc7565694
Josue Romero <jcromero@pucp.edu.pe>
2020-06-15 20:47:50 -0500

    josuerama

--- src/main/java/sw2/lab6/teletok/controller/PostController.java
+++ src/main/java/sw2/lab6/teletok/controller/PostController.java
@@ -1,13 +1,33 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import sw2.lab6.teletok.repository.PostRepository;
+
+import java.util.Date;
 
 @Controller
 public class PostController {
 
+    @Autowired
+    PostRepository postRepository;
+
     @GetMapping(value = {"", "/"})
-    public String listPost(){
+    public String listPost(Model model){
+        Date tudei = new Date();
+        model.addAttribute("listapost",postRepository.findAll());
+        model.addAttribute("hoydia",tudei);
+        return "post/list";
+    }
+
+    @PostMapping("post/search")
+    public String listPostSearch(@RequestParam("buscar") String search,Model model){
+        if (search.isEmpty()){
+            return "redirect:/";
+        }
+        model.addAttribute("listapost",postRepository.buscarPorDescripOUser(search));
         return "post/list";
     }
 
--- src/main/java/sw2/lab6/teletok/repository/PostRepository.java
+++ src/main/java/sw2/lab6/teletok/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package sw2.lab6.teletok.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sw2.lab6.teletok.entity.Post;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+
+        @Query(value="SELECT * FROM post where description =?1 or user_id = (select user_id from user where username = ?1)",nativeQuery = true)
+    List<Post> buscarPorDescripOUser(String search);
 }
